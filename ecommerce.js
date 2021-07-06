4d640e9a79553dadf1e8960651d47862be9e05ba
sathish <sathish.a@v2solutions.com>
2021-07-06 08:55:35 +0530

    removed command lines

--- ecommerce.js
+++ ecommerce.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const { check, validationResult } = require('express-validator');
-//const expressSession = require('express-session');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -25,22 +24,6 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//app.use(expressValidator());
-//app.use(expressSession({secret : 'max', saveUninitialized: false, resave : false}));
-
-
-// app.get('/', (req,res) => {
-//     let sqlCode = 'SELECT first_name, last_name FROM users';
-//     connection.query(sqlCode,
-//     function(err, results) {
-//         if(err){
-//             throw err;
-//         }
-//       res.render('database',{ name : results });
-//     }
-//   );
-// })
-
 app.post('/', [
   check('username','Username must be 3+ char long')
     .exists()
@@ -50,17 +33,6 @@ app.post('/', [
     .normalizeEmail(),
  
 ],(req,res) => {
-  // res.check('email', 'Invalid Email Address').isEmail();
-  // res.check('password', 'Password is Invalid').isStrongPassword({minLength: 5, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1});
-  // let errors = req.validationErrors();
-  // if(errors){
-  //   req.session.errors = errors;
-  //   req.session.success = false;
-  // }
-  // else{
-  //   req.session.success = true;
-  // }
-  // res.redirect('/signup');
   const errors = validationResult(req);
     if(!errors.isEmpty()){
       const alert = errors.array();
@@ -76,7 +48,6 @@ app.post('/', [
                 throw err;
             }
             for(let i=0;i<results.length;i++){
-              //if(results[i].username === username || results[i].password === password || results[i].email === email){
               if(results[i].email === email){
                 count++;
               }
@@ -87,31 +58,11 @@ app.post('/', [
             else{
               let sqlCode = `INSERT INTO customer(username,password,email) VALUES('${username}','${password}','${email}')`;
               connection.query(sqlCode);
-              // res.send(`<h3>SignUp Success</h3>
-              // <button onclick="history.go(-1)">
-              //   BACK
-              // </button>`);
               console.log(count);
               res.render('validation', { count });
             }
         }
       );
-
-
-
-
-
-
-
-  //console.log(username,password,email);
-  // let sqlCode = `INSERT INTO customers(username,password,email) VALUES('${username}','${password}','${email}')`;
-  // connection.query(sqlCode);
-  // function(err,results) {
-  //     if(err){
-  //         throw err;
-  //     }
-  //   res.send('SignUp successful');
-  // });
   });
 
 app.get('/', (req,res) => {
@@ -128,13 +79,11 @@ app.post('/user', (req,res) => {
             throw err;
         }
         for(let i=0;i<results.length;i++){
-          //if(results[i].username === username || results[i].password === password || results[i].email === email){
           if(results[i].email === email && results[i].password === password){
             count++;
           }
         }
         if(count > 0){
-          //res.render('validation',{count});
           res.sendFile(`${__dirname}/user.html`);
         }
         else{
@@ -142,18 +91,8 @@ app.post('/user', (req,res) => {
         }
     }
   );
-  //res.sendFile(`${__dirname}/user.html`);
 })
 
-
-// app.get('/login', (req,res) => {
-//   let sqlCode = 'SELECT first_name, last_name FROM users';
-//   connection.promise().query(sqlCode)
-//     .then( ([rows,fields]) => {
-//       res.render('database',{ name : rows});
-//     })
-//     .catch(console.log)
-//   });
 
 app.get('/login',(req,res) => {
   res.sendFile(`${__dirname}/login.html`)
@@ -161,8 +100,6 @@ app.get('/login',(req,res) => {
 
 app.get('/signup', (req,res) => {
   res.sendFile(`${__dirname}/signupForm.html`);
-  // res.render('signup',{success : req.session.success, errors : req.session.errors});
-  // req.session.errors = null;
 })
 
 
