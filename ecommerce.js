899eca11486b53796843a0c3792252987a631031
sathish <sathish.a@v2solutions.com>
2021-07-07 10:10:22 +0530

    change in js file and added public folder

--- ecommerce.js
+++ ecommerce.js
@@ -4,105 +4,93 @@ const bodyParser = require('body-parser');
 const { check, validationResult } = require('express-validator');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'sathish',
-    password:'Sathish@19',
-    database: 'ecommerce'
+  host: 'localhost',
+  user: 'sathish',
+  password: 'Sathish@19',
+  database: 'ecommerce',
 });
 
 connection.connect((err) => {
-    if(err){
-        throw err;
-    }
-    console.log('mysql is connected....');
-})
+  if (err) {
+    throw err;
+  }
+  console.log('mysql is connected....');
+});
 
 const app = express();
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
+app.set('views', `${__dirname}/public/views`);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/', [
-  check('username','Username must be 3+ char long')
+  check('username', 'Username must be 3+ char long')
     .exists()
-    .isLength({ min : 3 }),
+    .isLength({ min: 3 }),
   check('email', 'Invalid Email')
     .isEmail()
     .normalizeEmail(),
- 
-  ],(req,res) => {
+
+], (req, res) => {
   const errors = validationResult(req);
-    if(!errors.isEmpty()){
-      const alert = errors.array();
-      res.render('signup',{ alert });
-    }
-  
-  const { email, username, password } = req.body;
-  let check = 'select * from customer';
-  let count = 0;
-  connection.query(check,
-        function(err, results) {
-            if(err){
-                throw err;
-            }
-            for(let i=0;i<results.length;i++){
-              if(results[i].email === email){
-                count++;
-              }
-            }
-            if(count > 0){
-              res.render('validation',{count});
-            }
-            else{
-              let sqlCode = `INSERT INTO customer(username,password,email) VALUES('${username}','${password}','${email}')`;
-              connection.query(sqlCode);
-              console.log(count);
-              res.render('validation', { count });
-            }
+  if (!errors.isEmpty()) {
+    const alert = errors.array();
+    res.render('signup', { alert, alerts: 'Not' });
+  } else {
+    const { email, username, password } = req.body;
+    let count = 0;
+    connection.query('INSERT INTO customer(username,password,email) VAlUES (?, ?, ?)', [username, password, email],
+      (err) => {
+        if (err) {
+          count += 1;
         }
-  );
+        const errors = validationResult(req);
+        if (count > 0) {
+          const alert = errors.array();
+          res.render('signup', { alert, alerts: 'Exist' });
+        } else {
+          res.render('validation', { count });
+        }
+      });
+  }
 });
 
-app.get('/', (req,res) => {
-  res.sendFile(`${__dirname}/loginOrSignup.html`);
-})
+app.get('/', (req, res) => {
+  res.render('loginOrSignup');
+});
 
-app.post('/user', (req,res) => {
+app.post('/user', (req, res) => {
   const { email, password } = req.body;
-  let check = 'select * from customers';
+  const checkLogin = 'select * from customer';
   let count = 0;
-  connection.query(check,
-    function(err, results) {
-        if(err){
-            throw err;
+  connection.promise().query(checkLogin)
+    .then(([results, fields]) => {
+      for (let i = 0; i < results.length; i += 1) {
+        if (results[i].email === email && results[i].password === password) {
+          count += 1;
         }
-        for(let i=0;i<results.length;i++){
-          if(results[i].email === email && results[i].password === password){
-            count++;
-          }
-        }
-        if(count > 0){
-          res.sendFile(`${__dirname}/user.html`);
-        }
-        else{
-          res.send('Invalid Login');
-        }
-    }
-  );
-})
+      }
+      if (count > 0) {
+        res.render('user');
+      } else {
+        res.send('Invalid Login');
+      }
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
 
+app.get('/login', (req, res) => {
+  res.render('login');
+});
 
-app.get('/login',(req,res) => {
-  res.sendFile(`${__dirname}/login.html`)
-})
+app.get('/signup', (req, res) => {
+  res.render('signup', { alerts: 'hh' });
+});
 
-app.get('/signup', (req,res) => {
-  res.sendFile(`${__dirname}/signupForm.html`);
-})
-
-
-app.listen(3000, (req,res) => {
-    console.log('Listening on port 3000');
-})
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
